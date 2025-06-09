65133ac69033167639ebb564424fd1d4c341269c
angelgamo <garcia.angel.69420@gmail.com>
2025-06-09 21:17:07 +0200

    deploy triptic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.css';
+	import '$lib/fonts/fonts.css';
 
 	let { children } = $props();
 </script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="bg"></div>
+
+<div class="section1">
+  <div class="box">
+    <h1>Jardí Central</h1>
+    <h2>Sopars sota les estrelles</h2>
+    <p>Un espai exclusiu amb vistes al Tibidabo, ideal per viure sopars sota les estrelles amb show cooking en directe i música en viu. Amb zones privades i exclusives, combina gastronomia, natura i màgia en un entorn realment inoblidable.</p>
+    <div class="icon moon"></div>
+</div>
+  <div class="box box2"></div>
+  <div class="box f_right">
+    <h1>Botanic Club</h1>
+    <h2>Cocktails & Dreams</h2>
+    <p>Rodejat de vegetació i amb vistes increïbles a Collserola, gaudeix dels nostres còctels en un espai per somiar al caure el sol. </p>
+    <div class="icon drink"></div>
+  </div>
+  <div class="box">
+    <h1>Beer Garden</h1>
+    <h2>CHEERS</h2>
+    <p>La nostra carpa de la Cervesa és el nou espai perfecte per gaudir amb amics d’un ambient relaxat, cerveses ben fredes i una proposta gastronòmica per disfrutar d'una bona conversa a l’aire lliure.</p>
+    <div class="icon drink1"></div>  
+  </div>
+  <div class="box f_right">
+    <h1>Wine Bar</h1>
+    <h2>Degustació de vins</h2>
+    <p>Gaudeix d’una extensa selecció de vins i una proposta gastronòmica gourmet en un espai pensat per assaborir cada moment. La millor bodega, traslladada a l’espai ideal per gaudir-la com cal.</p>
+    <div class="icon drink2"></div>  
+  </div>
+</div>
+
+<div class="section2 *:text-center">
+    <div class="box flex flex-col gap-5">
+        <div>
+            <h1 class="font-[LeagueSpartan] text-[4rem] leading-none">QUATRE ESPAIS</h1>
+            <p class="font-[Trocchi] text-[2.7rem]">UNA <span class="!text-[#F2E9C8]">EXPERIÈNCIA</span></p>
+        </div>
+        <div class="bg-[#F2E9C8] border-2 rounded-xl relative">
+            <p class="font-[Trocchi] text-[2rem] !text-[#034F5A]">INAUGURACIÓ</p>
+            <p class="font-[Bakerie] text-[2rem] !text-black">Diumenge 15 juny a les 18.00hr</p>
+            <div class="w-15 h-15 absolute left-[-5px] top-[-10px] rotate-[-15deg] calendar"></div>
+        </div>
+        <div class="village w-full aspect-[1.257] border-2">
+            <p class="font-[LeagueSpartan] text-[2rem]">VILLAGE MASIA SANT CUGAT</p>
+        </div>
+    </div>
+    <div class="box">
+        <div>
+            <p class="font-[Bakerie] text-[3rem] !text-[#F2E9C8]">Sopars sota les estrelles</p>
+            <p class="font-[Trocchi] text-[2rem]">EN ESPAIS PRIVATS I EXCLUSIUS AL NOSTRE</p>
+        </div>
+        <div class="jardin-central w-full aspect-[1.3]"></div>
+        <p class="font-[Trocchi] text-[2rem]">SHOW COOKING & SOPARS AMBIENTATS</p>
+    </div>
+    <div class="box">
+        <div class="casa-fuster w-full aspect-[2]"></div>
+        <p class="font[LeagueSpartan] font-bold text-[3.2rem]">ESTÀS LLEST PER A L’ESTIU?</p>
+        <p class="font-[Bakerie] text-[2rem] text-start w-3/4 !text-[#F2E9C8]">Un nou concepte de Tardeo t’espera a la nostra Masia!</p>
+        <div class="casa-fuster2 w-full aspect-[1.836] border-2 bg-white relative">
+            <div class="tardeo w-40 h-40 absolute top-[-80px] right-[10px]"></div>
+        </div>
+    </div>
+</div>
+
+<div class="googleCalendar">
+    <iframe title="Calendar" src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FMadrid&showCalendars=0&showTz=0&showPrint=0&src=NTM3OWE4MGNjN2M5Mjc3Yzk1YjdhYWI4OTk2OWY0MDhmN2YyNDkwOTUxNDcwNDdlZmNkZTU1MmJkZTA4ZDhmM0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23ad1457" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</div>
